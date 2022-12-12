2b3b279d7b3ebaead3884c8dbcbd2e018612c5fe
S. Adithiya <93365795+Adithiya5Hub@users.noreply.github.com>
2022-12-12 13:03:21 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,18 +1,13 @@
 public class Calculator {
     import java.util.Scanner ;
-
     public static void main(String[] args) {
        Scanner in = new Scanner(System.in);
-
         int ans = 0;
-
         while (true) {
             System.out.print("Enter the operator: ");
             char op = in.next().trim().charAt(0);
-
             if (op == '+' || op == '-' || op == '*' || op == '/' || op == '%') {
                 System.out.print("Enter two numbers: ");
-
                 int num1 = in.nextInt();
                 int num2 = in.nextInt();
 
@@ -21,7 +16,6 @@ public class Calculator {
                 }
                 if (op == '-') {
                     ans = num1 - num2;
-
                 }
                 if (op == '*') {
                     ans = num1 * num2;
@@ -37,12 +31,8 @@ public class Calculator {
             } else {
                 System.out.println("Invalid operator: ");
             }
-
             System.out.println(ans);
         }
     }
-
-}
-
-
+  }
 }
